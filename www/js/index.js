1f681ad8c060c5218350bb49e35a2c8da6c2fe50
makevoid <makevoid@gmail.com>
2019-07-06 22:50:35 +0100

    browser wallet loads and transact - this keychain implementation is the same found in makevoid/ethereum-keychain

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
-const d = document
+const doc = document
 
 const bindButtons = () => {
-  // d.querySelector(".refresh-btn").addEventListener("click", refreshPage)
+  // doc.querySelector(".refresh-btn").addEventListener("click", refreshPage)
 }
 
 //  --------
@@ -20,7 +20,7 @@ main.defaultPage = "receive.html"
 main.currentPage = main.defaultPage
 
 main.open = () => {
-  const splitter = d.querySelector('#splitter')
+  const splitter = doc.querySelector('#splitter')
   splitter.open()
 }
 
@@ -31,8 +31,8 @@ main.setCurrentPage = (page) => (
 )
 
 main.load = (page) => {
-  const content  = d.querySelector('#content')
-  const splitter = d.querySelector('#splitter')
+  const content  = doc.querySelector('#content')
+  const splitter = doc.querySelector('#splitter')
   content.load(page)
     .then(splitter.close.bind(splitter))
     .then(bindButtons)
@@ -42,8 +42,8 @@ main.load = (page) => {
   // refactored:
   //
   // return (async () => {
-  //   const content  = d.querySelector('#content')
-  //   const splitter = d.querySelector('#splitter')
+  //   const content  = doc.querySelector('#content')
+  //   const splitter = doc.querySelector('#splitter')
   //   const contentIsLoaded = await content.load(page)
   //   splitter.close.bind(splitter)
   //   bindButtons()
@@ -57,3 +57,101 @@ main.refreshPage = () => {
   console.log(`refreshing ${main.currentPage}`)
   main.load(main.currentPage)
 }
+
+
+// TODO: merge
+
+
+class NullKeychain {}
+class NullBalance {}
+class NullRate {}
+
+class App {
+
+  constructor() {
+    this.keychain   = new NullKeychain()
+    this.balance    = new NullBalance()
+    this.balanceUsd = new NullBalance()
+    this.rate       = new NullRate()
+  }
+
+  initKeychain() {
+    console.log("init keychain")
+    const keychain = new Keychain({ store: localStorage })
+    this.keychain = keychain
+    // keychain.info()
+
+    ;(async () => {
+      // TODO: port back to keychain
+      const { balanceEth } = await keychain.netInfo()
+      // TODO: include rate into keychain
+      this.balance = balanceEth
+      this.updateBalanceUsd()
+
+
+      // require('EventEmitter')
+      // we take the eventEmitter functionality from an isolated dom node
+      const doc = document
+      const bodyElem  = doc.querySelector("html > body")
+      const domNode   = doc.createElement("div")
+      domNode.classList = ["events"]
+      bodyElem.appendChild(domNode) // div.events.hidden (empty div)
+      const eventsElem = doc.querySelector("html > body > div.events") // div.events.hidden (empty div)
+      const eventEmitter =  eventsElem
+      eventEmitter.on('balance', () => {
+        console.log('an evenbalance occurred!')
+      })
+      eventEmitter.emit('balance')
+      console.log(eventEmitter, eventsElem)
+
+      this.events = eventEmitter
+      this.events.updateBalances
+
+      // TODO: load cached value, load FX value from network later (10 seconds, or if everything else is loaded)
+      await app.loadFX()
+
+      // console.log("sendTXSelf")
+      // await keychain.sendTXSelf()
+
+      // console.log("netInfo")
+      // await keychain.netInfo()
+    })()
+  }
+
+  async loadFX() {
+    const price = await this.getDaiPrice()
+    this.rate = price
+    this.updateBalanceUsd()
+    console.log("Balance USD:", this.balanceUsd)
+  }
+
+  updateBalanceUsd() {
+    if (this.rate instanceof NullRate) return
+    this.balanceUsd = this.rate * this.balance
+  }
+
+  async getDaiPrice() {
+    let resp = await fetch("https://min-api.cryptocompare.com/data/price?fsym=DAI&tsyms=USD")
+    resp = await resp.json()
+    resp = resp.USD
+    console.log("DAIUSD rate fetched:", resp)
+    return resp
+  }
+
+  get address() {
+    return this.keychain.address
+  }
+
+  genNewAddress() {
+    return this.keychain.getNewAddress()
+  }
+
+  // get balance() {
+  //   return this.keychain.balance
+  // }
+
+}
+
+const app = new App()
+
+app.initKeychain()
